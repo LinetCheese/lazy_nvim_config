1cbd77e88a7ac5f53617c21443013f5007f55538
LinetCheese <linet_moreandil@pm.me>
2025-08-19 05:25:19 +0300

    added formatting and also snippets

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -26,5 +26,27 @@ return {
 		lspconfig.cland.setup { capabilities = capabilities }
 		lspconfig.ts_ls.setup { capabilities = capabilities }
 
+
+		vim.keymap.set("n", "<space>f", function()
+			vim.lsp.buf.format()
+		end)
+
+		-- On save
+		-- vim.api.nvim_create_autocmd('LspAttach', {
+		-- 	callback = function(args)
+		-- 		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		-- 		if not client then return end
+
+		-- @diagnostic disable-next-line: missing-parameter
+		-- 		if client.supports_method("textDocument/formatting") then
+		-- 			vim.api.nvim_create_autocmd('BufWritePre', {
+		-- 				buffer = args.buf,
+		-- 				callback = function()
+		-- 					vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+		-- 				end,
+		-- 			})
+		-- 		end
+		-- 	end
+		-- })
 	end,
 }
