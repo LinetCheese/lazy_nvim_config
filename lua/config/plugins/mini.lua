95fe21e8b0640fb5e854200efed98090ab429577
LinetCheese <linet_moreandil@pm.me>
2025-08-28 04:23:09 +0300

    added animated cursor

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -4,5 +4,9 @@ return 	{
 	config = function()
 		local statusline = require 'mini.statusline'
 		statusline.setup { use_icons = true }
+		local animated_cursor = require 'mini.animate'
+		animated_cursor.setup {
+			animated_cursor.gen_timing.linear({ duration = 100, unit = 'total' })
+		}
 	end
 }
